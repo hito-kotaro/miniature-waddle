6a2bad0c1516d5dcd5b9aadc6b539ff87fe07ddb
hito-kotaro <tohi.work@gmail.com>
2022-07-13 07:43:13 +0900

    マイグレーション関数をコメントアウト

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import db.models as models
 from db.database import engine
 from routes import auth_routes, user_routes, account_routes
 
-from db.migrate import reset_database
+# from db.migrate import reset_database
 
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
-reset_database()
+# reset_database()
 
 app.include_router(auth_routes.router)
 app.include_router(user_routes.router)
