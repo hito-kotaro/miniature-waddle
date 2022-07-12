9be695be2491134b902be21012ecd289be9598b1
hito-kotaro <tohi.work@gmail.com>
2022-07-12 09:12:16 +0900

    アカウントIDを実装

--- cruds/account.py
+++ cruds/account.py
@@ -1,3 +1,4 @@
+import random
 from sqlalchemy.orm import Session
 from db.models import Account
 from utils.hash import Hash
@@ -10,11 +11,24 @@ def get_account_by_email_query(db: Session, email: str):
     return db.query(Account).filter(Account.email == email).first()
 
 
+def get_account_by_id_query(db: Session, id: id):
+    """get user by email"""
+    print(id)
+    return db.query(Account).filter(Account.id == id).first()
+
+
 def create_account_query(db: Session, account: a_sc.AccountCreate):
     """create user by email and password"""
     hashed_password = Hash.get_password_hash(account.password)
 
+    while True:
+        account_id = random.randrange(10**8, 10**9)
+        if not get_account_by_id_query(db=db, id=account_id):
+            print("dup")
+            break
+
     new_account = Account(
+        id=account_id,
         email=account.email,
         hashed_password=hashed_password,
     )
--- routes/account_routes.py
+++ routes/account_routes.py
@@ -1,5 +1,4 @@
-from http.client import HTTPException
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 from cruds.account import get_account_by_email_query, create_account_query
 from schema import account_schema as a_sc
@@ -13,7 +12,7 @@ router = rb.create_router("account")
 
 
 @router.post("/create")
-def create_access_token(account: a_sc.AccountCreate, db: Session = Depends(rb.get_db)):
+def create_account(account: a_sc.AccountCreate, db: Session = Depends(rb.get_db)):
     db_account = get_account_by_email_query(db=db, email=account.email)
     if db_account:
         raise HTTPException(status_code=400, detail="account already registerd")
