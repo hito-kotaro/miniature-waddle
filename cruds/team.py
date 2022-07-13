0700dfde566eff3b5fbc904bb4cd0ea3549ffd27
hito-kotaro <tohi.work@gmail.com>
2022-07-13 21:04:33 +0900

    CORS確認

--- cruds/team.py
+++ cruds/team.py
@@ -13,10 +13,22 @@ def get_team_by_id(db: Session, account_id: int, team_name: str):
     return team
 
 
-def create_team_query(db: Session, account_id: int, team_name: str, description: str):
+def create_team(db: Session, account_id: int, team_name: str, description: str):
     team = Team(name=team_name, description=description, account_id=account_id)
-    print(team)
     db.add(team)
     db.commit()
     db.refresh(team)
+    return team
+
+
+def get_all_team(db: Session, account_id: int):
+    teams = db.query(Team).filter(Team.account_id == account_id).all()
+    return {"teams": teams}
+
+
+def get_user_by_id(db: Session, account_id: int, team_id: int):
+    team = (
+        db.query(Team).filter(Team.account_id == account_id, Team.id == team_id).first()
+    )
+
     return team
--- routes/team_routes.py
+++ routes/team_routes.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
-from cruds.team import get_team_by_id, create_team_query
+from cruds.team import get_team_by_id, create_team, get_all_team, get_user_by_id
 import schema.team_schema as t_sc
 from routes import router_base as rb
 from cruds.auth import get_current_user
@@ -12,19 +12,19 @@ router = rb.create_router("team")
 
 
 @router.post("/create")
-def create_user_api(
-    create_team: t_sc.CreateTeam,
+def create_team_api(
+    team: t_sc.CreateTeam,
     db: Session = Depends(rb.get_db),
     current_user: str = Depends(get_current_user),
 ):
     # アカウント内での重複確認
     team = get_team_by_id(
-        db=db, account_id=current_user.account_id, team_name=create_team.name
+        db=db, account_id=current_user.account_id, team=create_team.name
     )
-    print(team)
     if team:
         raise HTTPException(status_code=400, detail="team already exists")
-    return create_team_query(
+
+    return create_team(
         db=db,
         account_id=current_user.account_id,
         team_name=create_team.name,
@@ -33,6 +33,19 @@ def create_user_api(
 
 
 # 全てのチームを取得
+@router.get("/", response_model=t_sc.ResponseTeams)
+def get_all_team_api(
+    db: Session = Depends(rb.get_db),
+    current_user: str = Depends(get_current_user),
+):
+    return get_all_team(db=db, account_id=current_user.account_id)
 
 
 # 特定のIDのチームを取得
+@router.get("/{id}", response_model=t_sc.ResponseTeam)
+def get_team_api(
+    id: int,
+    db: Session = Depends(rb.get_db),
+    current_user: str = Depends(get_current_user),
+):
+    return get_user_by_id(db=db, account_id=current_user.account_id, team_id=id)
